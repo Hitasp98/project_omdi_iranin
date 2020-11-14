c79ccef2fb145281da2e78ad964cdc4ea067d75d
armin nouri <arminoori52@yahoo.com>
2020-11-14 17:16:25 +0330

    add file

--- main1.py
+++ main1.py
@@ -26,7 +26,7 @@ class pro1():
 #2.7 dar akharm nmaysh
         print(ss)
 #2.8sakht method save file 
-        with open("test.text","w") as f:
+        with open("test.bat","w") as f:
                 f.write(ss)
 # 3 sakht class pro
 class pro(pro1):
@@ -35,27 +35,8 @@ class pro(pro1):
         self.a=a    
 # 5 method jaygozari khargosh va sang va havig ro misazim
     def mohasbe(self):          
-# 6 sakht halgh for 11tayy  barya in ke goft shode 11 ta az harkodom
-        for i in range(11):
-# 7 sakht add random 1 ta 100 
-            r2=random.randint(1,100)
-# 8 ta vagh barabar nabod ' ' dar jayy halgh  
-            while self.a[r2]!=' ':
-                r2=random.randint(1,100)
-# 9 agr bod s ro bezar
-            self.a[r2]="s"
-# 10 ta vagh barabar nabod ' ' dar jayy halgh  
-            while self.a[r2]!=' ':
-# 11 sakht add random 1 ta 100 
-                r2=random.randint(1,100)
-# 12 agr bod c ro bezar
-            self.a[r2]="c"
-# 13 ta vagh barabar nabod ' ' dar jayy halgh  
-            while self.a[r2]!=' ':
-# 14 sakht add random 1 ta 100 
-                r2=random.randint(1,100)
-# 15 agr bod R ro bezar
-            self.a[r2]="R"
+
+      
 #16 bro barye nmaysh class pro1 seda kon
         n=pro1(self.a)
 #17 az method show barye namysh estfade kon
@@ -63,10 +44,72 @@ class pro(pro1):
 
 # 1.1 skht arye 101 tayy bekhter ziba namysh dadn 101 shod
 a=[' ' for x in range(101)] 
-#1.2 frstadn arrye a be class pro
-d=pro(a)
-#1.3 farkhani method mohasbe 
-d.mohasbe()
+# 6 sakht halgh for 11tayy  barya in ke goft shode 11 ta az harkodom
+for i in range(11):
+    # 7 sakht add random 1 ta 100 
+            r2=random.randint(1,100)
+# 8 ta vagh barabar nabod ' ' dar jayy halgh  
+            while a[r2]!=' ':
+                r2=random.randint(1,100)
+# 9 agr bod s ro bezar
+            a[r2]="s"
+# 10 ta vagh barabar nabod ' ' dar jayy halgh  
+            while a[r2]!=' ':
+# 11 sakht add random 1 ta 100 
+                r2=random.randint(1,100)
+# 12 agr bod c ro bezar
+            a[r2]="c"
+# 13 ta vagh barabar nabod ' ' dar jayy halgh  
+            while a[r2]!=' ':
+# 14 sakht add random 1 ta 100 
+                r2=random.randint(1,100)
+# 15 agr bod R ro bezar
+            a[r2]="R"
+#2.3 sakht skht string bary rekhtan arrye vorodi braye namayesh
+ss=""
+#2.4 sakht halgh bar mortb rikhtn arrey be string
+for s in range(1,101):
+#2.5 bary in ke 10 dar 10 besh age halge be s%10==0 rsid ber khat bad ba ezaf kardn \n
+            if s%10==0:
+                ss+="\t"+a[s]
+                ss+="\n"
+#2.6 dar gher in sorat ba \t ezafe kone 
+            else: 
+                ss+="\t"+a[s]
+#2.7 dar akharm nmaysh
+print(ss)
+#2.8sakht method save file 
+with open("test.bat","w") as f:
+                f.write(ss)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #1.2 frstadn arrye a be class pro
+# d=pro(a)
+# #1.3 farkhani method mohasbe 
+# d.mohasbe()
 
 
 #end
---
+++ test.bat
@@ -0,0 +1,10 @@
+	R	 	 	s	 	R	 	 	 	 
+	 	 	 	 	 	 	 	s	 	 
+	 	 	R	s	 	 	c	 	 	 
+	 	R	c	 	 	 	 	 	 	s
+	 	c	 	R	 	s	R	R	 	 
+	 	 	 	 	R	 	 	c	 	R
+	R	c	 	c	 	 	 	 	 	 
+	 	s	 	 	 	 	 	s	 	c
+	c	 	s	 	 	 	 	 	s	 
+	R	 	s	 	 	s	c	c	 	c
